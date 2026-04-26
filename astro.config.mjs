08b09e7ba0bfd00b760db5e88ab6de58377dbd7e
addithyan <adithyanb12b@gmail.com>
2026-04-27 02:46:14 +0530

    fix: add nojekyll and correct workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://addithyan.github.io',
-  base: '/my-portfolio',
   integrations: [tailwind()],
   output: 'static',
 });
